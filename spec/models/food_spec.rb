8a35ca2daecc5e3bafec91dadfd359b76da95c40
Jordan <jordan.wallace.williams@gmail.com>
2020-07-23 21:31:42 -0600

    Add Food spec

---
+++ spec/models/food_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :amount }
+    it { should validate_presence_of :unit_type }
+    it { should validate_presence_of :expiration }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :farm }
+  end
+end
