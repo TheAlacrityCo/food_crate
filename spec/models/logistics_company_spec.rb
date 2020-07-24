0e3daabc92ba2cda91479cdd74bc654246a8d877
Jordan <jordan.wallace.williams@gmail.com>
2020-07-23 21:34:41 -0600

    Add LogisticsCompany spec

---
+++ spec/models/logistics_company_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe LogisticsCompany, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :country }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :phone }
+    it { should validate_presence_of :max_load }
+  end
+end
