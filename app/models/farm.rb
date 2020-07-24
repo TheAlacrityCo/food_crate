369fd2347f78d1e7efc879610e0df20e28ddc58e
Jordan <jordan.wallace.williams@gmail.com>
2020-07-23 21:29:42 -0600

    Add Farm model

---
+++ app/models/farm.rb
@@ -0,0 +1,12 @@
+class Farm < ApplicationRecord
+  validates :name, presence: true
+  validates :state, presence: true
+  validates :country, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
+  validates :need_level, presence: true
+
+  enum need_level: %i[low medium high]
+
+  has_many :foods
+end
