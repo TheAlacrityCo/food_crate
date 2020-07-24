a921c43ff07cceec37235f2fd5696af17b0247be
Jordan <jordan.wallace.williams@gmail.com>
2020-07-23 21:30:25 -0600

    Add Farm spec

---
+++ spec/models/farm_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Farm, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :country }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :phone }
+    it { should validate_presence_of :need_level }
+  end
+
+  describe 'relationships' do
+    it { should have_many :foods }
+  end
+end
