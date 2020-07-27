806f668f35ef67fec85e009d412d89c5ff7b4132
Jordan <jordan.wallace.williams@gmail.com>
2020-07-27 14:48:24 -0600

    Add polymorphic relationship: has_many :users as: :employable

--- app/models/logistics_company.rb
+++ app/models/logistics_company.rb
@@ -5,4 +5,6 @@ class LogisticsCompany < ApplicationRecord
   validates :address, presence: true
   validates :phone, presence: true
   validates :max_load, numericality: { only_integer: true }, presence: true
+
+  has_many :users, as: :employable
 end
