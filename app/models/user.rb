209e698b4ff6f18fd16df29d4a6a80c61734f7e2
Jordan <jordan.wallace.williams@gmail.com>
2020-07-27 14:50:23 -0600

    Update model relationships: belongs_to :employable, ploymorphic: true

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
+
+  belongs_to :employable, polymorphic: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :email }
     it { should validate_presence_of :encrypted_password }
   end
+
+  describe 'relationships' do
+    it { should belong_to :employable }
+  end
 end
