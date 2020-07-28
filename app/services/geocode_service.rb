825eb72e740178358d7f71348633f2811ed32b89
David Atkins <atki1080@gmail.com>
2020-07-28 16:32:11 -0600

    Add geocode service

---
+++ app/services/geocode_service.rb
@@ -0,0 +1,16 @@
+class GeocodeService
+    def self.get_coordinates(address)
+        response = conn.get do |req|
+            req.params['address'] = address
+        end
+        JSON.parse(response.body, symbolize_names: true)[:results].first[:geometry][:location]
+    end
+
+    private
+        
+        def self.conn
+            Faraday.new "https://maps.googleapis.com/maps/api/geocode/json" do |conn|
+                conn.params['key'] = ENV['google_api_key']
+            end
+        end
+end
