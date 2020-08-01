d4d89d47b24995f2d2b4964261d207be320f542c
Jordan <jordan.wallace.williams@gmail.com>
2020-08-01 15:09:10 -0600

    Update entrypoint_production.sh to include starting Rails server

--- entrypoint_production.sh
+++ entrypoint_production.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
+
 cd /farm_link
 
 # Create the Rails production DB on first run
@@ -9,9 +11,9 @@ RAILS_ENV=production bundle exec rake db:create
 RAILS_ENV=production bundle exec rake db:migrate
 
 # Do some protective cleanup
-> log/production.log
 rm -f /farm_link/tmp/pids/server.pid
 
 # Run the web service on container startup
 # $PORT is provided as an environment variable by Cloud Run
-bundle exec rails server -e production -b 0.0.0.0 -p 3000
+# exec "$@"
+bundle exec rails server -e production -b 0.0.0.0 -p $PORT
