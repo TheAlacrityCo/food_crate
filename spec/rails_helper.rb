34ea0ca8c25c0eb550ecee9d646c59b7469c1b0e
Jordan <jordan.wallace.williams@gmail.com>
2020-08-09 00:10:57 -0600

    Add Coveralls setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
 require 'spec_helper'
+require 'coveralls'
 
-SimpleCov.start 'rails'
+SimpleCov.start('rails')
+Coveralls.wear!('rails')
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
