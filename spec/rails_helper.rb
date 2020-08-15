9b3e425784ff57ada27aa5deedd80329bf065b0f
Jordan <jordan.wallace.williams@gmail.com>
2020-08-14 21:55:21 -0600

    Add config to login with Devise when using RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,6 +55,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Login with Devise
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
 
 Shoulda::Matchers.configure do |config|
