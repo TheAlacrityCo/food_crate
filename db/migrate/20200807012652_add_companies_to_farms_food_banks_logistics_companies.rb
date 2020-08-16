067c7c6cd04f5babc9406fdab6f5bfc2d5e2d511
Jordan <jordan.wallace.williams@gmail.com>
2020-08-15 21:23:37 -0600

    Delete migration with user relationships that are no longer necessary

--- db/migrate/20200807012652_add_companies_to_farms_food_banks_logistics_companies.rb
+++
@@ -1,7 +0,0 @@
-class AddCompaniesToFarmsFoodBanksLogisticsCompanies < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :farms, :companies, polymorphic: true
-    add_reference :food_banks, :companies, polymorphic: true
-    add_reference :logistics_companies, :companies, polymorphic: true
-  end
-end
