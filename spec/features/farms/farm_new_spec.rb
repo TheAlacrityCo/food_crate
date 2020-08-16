ae601a983f73718269e4b37d3f141a74159d3c74
Jordan <jordan.wallace.williams@gmail.com>
2020-08-16 13:32:36 -0600

    Update spec that tests creating a new farm to pass all tests

--- spec/features/farms/farm_new_spec.rb
+++ spec/features/farms/farm_new_spec.rb
@@ -3,30 +3,27 @@ require 'rails_helper'
 RSpec.describe 'Create a new Farm resource as a user' do
   before :each do
     @user = create(:user)
-    # sign_in @user
-
-    visit '/users/sign_in'
-    fill_in 'user[email]', with: @user.email
-    fill_in 'user[password]', with: @user.encrypted_password
-    click_on 'Log in'
-
+    sign_in @user
   end
 
-  xit 'There is a form on the new Farm view to create a farm resource' do
+  it 'There is a form on the new Farm view to create a farm resource' do
     visit root_path
 
     expect(page).to have_link('Add a Farm')
     click_on 'Add a Farm'
 
     expect(current_path).to eq('/farms/new')
+    expect(page).to have_content(@user.email)
 
-    fill_in :name, with: 'The Garden Patch'
-    fill_in :state, with: 'Maine'
-    fill_in :country, with: 'US'
-    fill_in :address, with: '321 Veggie Drive'
-    fill_in :phone, with: '321-654-0987'
-    fill_in :need_level, with: 1
+    fill_in 'farm[name]', with: 'The Garden Patch'
+    fill_in 'farm[state]', with: 'Maine'
+    fill_in 'farm[country]', with: 'US'
+    fill_in 'farm[address]', with: '321 Veggie Drive'
+    fill_in 'farm[phone]', with: '321-654-0987'
+    choose 'farm_need_level_low'
 
-    click 'Create Farm'
+    click_on 'Create Farm'
+
+    expect(current_path).to eq('/farms')
   end
 end
