ae370f92cea76bfd9572cdca4949086998c6eb09
Jordan <jordan.wallace.williams@gmail.com>
2020-08-16 19:25:27 -0600

    Add sad path tests to ensure that only registered users can create farms

--- spec/features/farms/farm_new_spec.rb
+++ spec/features/farms/farm_new_spec.rb
@@ -55,4 +55,25 @@ RSpec.describe 'Create a Farm' do
     expect(page).to have_content("Phone can't be blank")
     expect(page).to have_content("Need level can't be blank")
   end
+
+  it 'can only be created by registered users' do
+    visit new_farm_path
+
+    expect(page).to have_field('farm[name]')
+    expect(page).to have_field('farm[country]')
+    expect(page).to have_field('farm[state]')
+    expect(page).to have_field('farm[address]')
+    expect(page).to have_field('farm[phone]')
+
+    sign_out @user
+
+    visit new_farm_path
+
+    expect(current_path).to eq(new_user_session_path)
+    expect(page).to have_content("You need to sign in or sign up before continuing.")
+    expect(page).to have_content('Log in')
+    expect(page).to have_field('user[email]')
+    expect(page).to have_field('user[password]')
+    expect(page).to have_button('Log in')
+  end
 end
