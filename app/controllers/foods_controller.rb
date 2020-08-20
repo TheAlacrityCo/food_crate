587c73306d01152564267c02b313697f49c11894
Jordan <jordan.wallace.williams@gmail.com>
2020-08-20 17:26:54 -0600

    Update foods_controller.rb with logic for creating new foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,9 +1,30 @@
 class FoodsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
-    # new food
+    @food = Food.new
   end
 
   def create
-    # create food
+    food = Food.new(food_params)
+    if food.save
+      flash[:success] = 'Food created successfully.'
+      redirect_to farm_foods_items_path
+    else
+      flash[:warning] = food.errors.full_messages.to_sentence + '.'
+      redirect_to new_farm_food_path
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(
+      :name,
+      :amount,
+      :unit_type,
+      :expiration,
+      :farm_id
+    )
   end
 end
